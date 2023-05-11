28119ccce134ebdde1178c5f6cddf2eac44c5ff9
MRMilton912 <multimillionairemilton@gmail.com>
2023-05-11 13:14:06 -0400

    Server

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const http = require('http');
+fs = require('fs');
+url = require('url');
 
 http.createServer((request, response) => {
+  let addr = request.url;
+  q = url.parse(addr, true);
+  filePath = '';
+  if (q.pathname.includes('documentation')) {
+    filePath = (_dirname = '/documentation.html');
+  } else {
+    filePath = 'index.html'
+  }
+
+  fs.appendFile('log.txt','URL:' +addr+ '\nGTimestamp:' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.group(err);
+    }
+  });
+  
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello Node!\n');
+
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
